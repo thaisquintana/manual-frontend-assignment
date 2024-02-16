4b17a0fe32beaf93f383f481677be9d9d93e700c
Thais Quintana <thaisquintanap@gmail.com>
2024-02-16 20:31:36 -0300

    added eslint and prettier rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image'
+import styles from './page.module.css'
+import Script from 'next/script'
 
 export default function Home() {
+  const foo = 'bla'
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -15,7 +17,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -91,5 +93,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  );
+  )
 }
