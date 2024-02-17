932163bb7baea15fbb2d12d9933aee022f99ae30
Thais Quintana <thaisquintanap@gmail.com>
2024-02-16 23:43:04 -0300

    added storybook

---
+++ src/components/banner/hero/stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { HeroBanner } from '.'
+
+export default {
+  title: 'components/HeroBanner',
+  component: HeroBanner,
+  parameters: {
+    layout: 'fullScreen'
+  }
+} as Meta
+
+export const Default: StoryObj = {}
