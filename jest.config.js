ea480f8f7b8d23b6a8e275db0cdb72941d6fa648
Thais Quintana <thaisquintanap@gmail.com>
2024-02-17 16:38:42 -0300

    fix config importation of absolute paths on test files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: [
@@ -11,15 +12,18 @@ module.exports = {
     '!src/styles/**'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   },
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  modulePaths: ['<rootDir>/src/'],
   // https://github.com/styled-components/styled-components/issues/4081
   // v6 of styled-components doesn't inject styles in test environment
   // we should to force it to use the browser version
   moduleNameMapper: {
     '^styled-components':
-      'styled-components/dist/styled-components.browser.cjs.js'
+      'styled-components/dist/styled-components.browser.cjs.js',
+    '^.+\\.svg$': 'jest-svg-transformer',
+    '^@/(.*)$': '<rootDir>/src/$1'
   }
 }
