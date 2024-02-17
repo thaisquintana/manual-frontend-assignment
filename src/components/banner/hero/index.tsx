68762311daa8104e2de62dc3123142bc3abde2c5
Thais Quintana <thaisquintanap@gmail.com>
2024-02-17 16:50:08 -0300

    fix test and add Button to the heroBanner component

--- src/components/banner/hero/index.tsx
+++ src/components/banner/hero/index.tsx
@@ -1,11 +1,19 @@
+import { Button } from '@/components/button'
 import { Content } from './styles'
 
-export const HeroBanner = () => (
-  <Content data-testid="hero-content">
-    <h1>Be good to yourself</h1>
-    <p>
-      We’re working around the clock to bring you a holistic approach to your
-      wellness. From top to bottom, inside and out.
-    </p>
-  </Content>
-)
+export const HeroBanner = () => {
+  const handleClick = () => {
+    console.log('teste')
+  }
+
+  return (
+    <Content data-testid="hero-content">
+      <h1>Be good to yourself</h1>
+      <p>
+        We’re working around the clock to bring you a holistic approach to your
+        wellness. From top to bottom, inside and out.
+      </p>
+      <Button color="#7E0707" onClick={handleClick} text="Take the quiz" />
+    </Content>
+  )
+}
--- src/components/banner/hero/test.tsx
+++ src/components/banner/hero/test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import { HeroBanner } from '.'
+import userEvent from '@testing-library/user-event'
 
 describe('<HeroBanner />', () => {
   it('should render correctly', () => {
-    const { container } = render(<HeroBanner />)
-
+    render(<HeroBanner />)
     expect(
       screen.getByRole('heading', { name: /Be good to yourself/i })
     ).toBeInTheDocument()
@@ -13,9 +13,13 @@ describe('<HeroBanner />', () => {
         'We’re working around the clock to bring you a holistic approach to your wellness. From top to bottom, inside and out.'
       )
     ).toBeInTheDocument()
-    expect(container.firstChild).toHaveStyle({ 'background-color': '#a5b79f' })
+    expect(screen.getByTestId('hero-content')).toHaveStyle({
+      'background-color': '#a5b79f'
+    })
     expect(screen.getByTestId('hero-content')).toHaveStyle({
       'background-image': 'url("assets/image-herobanner.webp")'
     })
+    const button = screen.getByText(/Take the quiz/i)
+    userEvent.click(button)
   })
 })
